44db02a0a737dba01bad4bac5bace28b0b679e4a
Jony Yang <yangjx@legal.regn.net>
2017-12-19 18:54:34 +0800

    Ajax by Fetch method

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './App.css';
+import TestFetch from './TestFetch'
 
 //const logo = require('./logo.svg');
 
@@ -14,6 +15,7 @@ class App extends React.Component {
         <p className="App-intro" >
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p> */}
+        <TestFetch />
       </div>
     );
   }
