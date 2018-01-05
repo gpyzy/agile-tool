af41df00325d54636fdc6b2e8447dc191cba61f8
Jony Yang <yangjx@legal.regn.net>
2018-01-05 14:56:55 +0800

    no message

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 import { Login, UserInfo, helloWorld } from '../Login';
-import { Button } from 'antd/lib/radio';
 
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
@@ -30,10 +29,12 @@ class App extends React.Component<AppProps> {
             <Menu.Item key="1">nav 1</Menu.Item>
             <Menu.Item key="2">nav 2</Menu.Item>
             <Menu.Item key="3">
-              <Login helloWorld={(input: string) => dispatch(helloWorld(input))} />
+              <Login
+                helloWorld={(input: string) => dispatch(helloWorld(input))}
+              />
             </Menu.Item>
             <Menu.Item key="4">
-            <Button></
+              <Button>Get List</Button>
             </Menu.Item>
           </Menu>
         </Header>
@@ -45,19 +46,40 @@ class App extends React.Component<AppProps> {
               defaultOpenKeys={['sub1']}
               style={{ height: '100%', borderRight: 0 }}
             >
-              <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+              <SubMenu
+                key="sub1"
+                title={
+                  <span>
+                    <Icon type="user" />subnav 1
+                  </span>
+                }
+              >
                 <Menu.Item key="1">option1</Menu.Item>
                 <Menu.Item key="2">option2</Menu.Item>
                 <Menu.Item key="3">option3</Menu.Item>
                 <Menu.Item key="4">option4</Menu.Item>
               </SubMenu>
-              <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
+              <SubMenu
+                key="sub2"
+                title={
+                  <span>
+                    <Icon type="laptop" />subnav 2
+                  </span>
+                }
+              >
                 <Menu.Item key="5">option5</Menu.Item>
                 <Menu.Item key="6">option6</Menu.Item>
                 <Menu.Item key="7">option7</Menu.Item>
                 <Menu.Item key="8">option8</Menu.Item>
               </SubMenu>
-              <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
+              <SubMenu
+                key="sub3"
+                title={
+                  <span>
+                    <Icon type="notification" />subnav 3
+                  </span>
+                }
+              >
                 <Menu.Item key="9">option9</Menu.Item>
                 <Menu.Item key="10">option10</Menu.Item>
                 <Menu.Item key="11">option11</Menu.Item>
@@ -71,9 +93,16 @@ class App extends React.Component<AppProps> {
               <Breadcrumb.Item>List</Breadcrumb.Item>
               <Breadcrumb.Item>App</Breadcrumb.Item>
             </Breadcrumb>
-            <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+            <Content
+              style={{
+                background: '#fff',
+                padding: 24,
+                margin: 0,
+                minHeight: 280
+              }}
+            >
               Content
-        </Content>
+            </Content>
           </Layout>
         </Layout>
       </Layout>
