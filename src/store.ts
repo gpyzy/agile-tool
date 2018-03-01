94332a3a7345db74b74b18cbcc6c62cf833e92fd
Jony Yang <jony@live.cn>
2018-03-01 16:20:45 +0800

    Add comments
    (cherry picked from commit 3cc3ab7fee230e8f67e5ed898c634949fa02c839)

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,7 @@
+// take reference from https://github.com/facebook/create-react-app/issues/2317
+// need to run 'yarn add -D @types/webpack-env' to install the webpack-env for typescript 
+// or else compile not get success.
+
 import { Store, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducer';
