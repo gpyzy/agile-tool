9799dbcec8b083eae4a26bbec04d78fea3d78059
Jony Yang <jony@live.cn>
2018-03-01 18:00:31 +0800

    prepare for token middleware.

--- src/middlewares/oauth2-token-middleware.ts
+++ src/middlewares/oauth2-token-middleware.ts
@@ -1,10 +1,15 @@
 import { MiddlewareAPI } from 'redux';
-// import * as _ from 'lodash';
+import * as Cookies from 'es-cookie';
+import { get as lodash_get } from 'lodash';
 
-const oauth2TokenMiddleware = fetchImplementation => (store: MiddlewareAPI<{}>) => {
+const oauth2TokenMiddleware = fetchImplementation => (
+  store: MiddlewareAPI<{}>
+) => {
   return next => {
     return action => {
-      console.log('haha?');
+      const tokenInCookie = Cookies.get('token');
+      const tokenInState = lodash_get(store.getState(), 'part3.token');
+
       return next(action);
     };
   };
