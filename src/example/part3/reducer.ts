5d5ea19f35ccdc05da0ecf7440c2aaabdfbcc0bd
Jony Yang <yangjx@legal.regn.net>
2018-03-02 14:50:55 +0800

    rename variables

--- src/example/part3/reducer.ts
+++ src/example/part3/reducer.ts
@@ -1,9 +1,9 @@
 import { handleActions, Action, handleAction } from 'redux-actions';
 import {
-  GET_USER_CLICK,
+  GET_USERS,
   GOT_USERS,
-  REFRESH_TOKEN_CLICK,
-  REFRESHED_TOKEN
+  GET_TOKEN,
+  GOT_TOKEN
 } from './action-types';
 import User from '../../Models/User';
 import Part3State from './state';
@@ -25,7 +25,7 @@ const gotUsers = handleAction<Part3State, User[]>(
 );
 
 const getUserClick = handleAction<Part3State, number>(
-  GET_USER_CLICK,
+  GET_USERS,
   (state, action) => ({
     ...state,
     clickCount: state.clickCount + (action.payload as number)
@@ -34,13 +34,13 @@ const getUserClick = handleAction<Part3State, number>(
 );
 
 const refereshTokenClick = handleAction<Part3State, number>(
-  REFRESH_TOKEN_CLICK,
+  GET_TOKEN,
   (state, action) => state,
   initialState
 );
 
 const refreshedToken = handleAction<Part3State, string>(
-  REFRESHED_TOKEN,
+  GOT_TOKEN,
   (state, action) => ({
     ...state,
     token: action.payload as string
@@ -51,9 +51,9 @@ const refreshedToken = handleAction<Part3State, string>(
 const part3 = handleActions<Part3State, number | User[] | string>(
   {
     [GOT_USERS]: gotUsers,
-    [GET_USER_CLICK]: getUserClick,
-    [REFRESH_TOKEN_CLICK]: refereshTokenClick,
-    [REFRESHED_TOKEN]: refreshedToken
+    [GET_USERS]: getUserClick,
+    [GET_TOKEN]: refereshTokenClick,
+    [GOT_TOKEN]: refreshedToken
   },
   initialState
 );
